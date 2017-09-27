9fa788141010216cd1c7b22900ab933d27a18a16
marnidevlabs <marni@creativedevlabs.com>
2017-09-27 22:48:07 +0800

    tourist view hover and scroll effect

--- assets/app/controller/homeController.js
+++ assets/app/controller/homeController.js
@@ -4,15 +4,15 @@ app.controller('homeController',[
     '$scope',
     function($scope)
     {
-        $("section .navbar .container .navbar-header button").click(function(){
+        $(".navbar .container .navbar-header button").click(function(){
             $(".navbar .container .navbar-header button").addClass("click");
             $("#mobileview #navmenu").addClass("showmobile");
         
         });
-        $("section #mobileview #navmenu button").click(function(){
+        $("#mobileview #navmenu button").click(function(){
             $("#mobileview #navmenu").removeClass("showmobile");
         });
-        $("section #bannerimage").slick({
+        $("#bannerimage").slick({
             arrows: false,
             dots: true
         });
@@ -21,13 +21,55 @@ app.controller('homeController',[
             dots: true
         });
         function collapsenav(){
-            if ($("section .navbar").offset().top > 80){
-                $('section .navbar').addClass("scroll-change");
+            if ($(".navbar").offset().top > 80){
+                $('.navbar').addClass("scroll-change");
             }else{
-                $('section .navbar').removeClass("scroll-change");
+                $('.navbar').removeClass("scroll-change");
             }
         };
         $(window).scroll(collapsenav);
         $(document).ready(collapsenav);
+        $(document).ready(function(){
+            $("#tourist #spot #image-left .imagecontent .imagehover .info button").on("click",function(){
+                var x = $(this).attr("data-image");
+                var des = $(this).prev().html();
+                $("#view img").attr("src",x);
+                $("#view #box2 p").html(des);
+                var num = $("#view").offset().top - 50 
+                $('html,body').animate({
+                    scrollTop: num
+                }, 600);
+            });
+            $("#tourist #spot #image-right .imagecontent .imagehover .info button").on("click",function(){
+                var x = $(this).attr("data-image");
+                var des = $(this).prev().html();
+                $("#view img").attr("src",x);
+                $("#view #box2 p").html(des);
+                var num = $("#view").offset().top - 50 
+                $('html,body').animate({
+                    scrollTop: num
+                }, 600);
+            });
+            $("#tourist #spot #center-image .imagehover .info button").on("click",function(){
+                var x = $(this).attr("data-image");
+                var des = $(this).prev().html();
+                $("#view img").attr("src",x);
+                $("#view #box2 p").html(des);
+                var num = $("#view").offset().top - 50 
+                $('html,body').animate({
+                    scrollTop: num
+                }, 600);
+            });
+            $("#tourist #spot2 .image-content .imagehover .info button").on("click",function(){
+                var x = $(this).attr("data-image");
+                var des = $(this).prev().html();
+                $("#view img").attr("src",x);
+                $("#view #box2 p").html(des);
+                var num = $("#view").offset().top - 50 
+                $('html,body').animate({
+                    scrollTop: num
+                }, 600);
+            });
+        });
     }
 ]);
