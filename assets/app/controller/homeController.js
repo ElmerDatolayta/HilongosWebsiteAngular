c65d62a0afafd67acc93ea0dc6a4876b1384db17
DESKTOP-O6M8492\Elmer Datolayta <e.datolaytz@gmail.com>
2017-09-29 22:05:57 +0800

    Resolve Conflict

--- assets/app/controller/homeController.js
+++ assets/app/controller/homeController.js
@@ -4,32 +4,12 @@ app.controller('homeController',[
     '$scope',
     function($scope)
     {
-        $(".navbar .container .navbar-header button").click(function(){
-            $(".navbar .container .navbar-header button").addClass("click");
-            $("#mobileview #navmenu").addClass("showmobile");
         
-        });
-        $("#mobileview #navmenu button").click(function(){
-            $("#mobileview #navmenu").removeClass("showmobile");
-        });
-        $("#bannerimage").slick({
-            arrows: false,
-            dots: true,
-            autoplay:true
-        });
         $("section #about #bannerimage").slick({
             arrows: false,
             dots: true
         });
-        function collapsenav(){
-            if ($(".navbar").offset().top > 80){
-                $('.navbar').addClass("scroll-change");
-            }else{
-                $('.navbar').removeClass("scroll-change");
-            }
-        };
-        $(window).scroll(collapsenav);
-        $(document).ready(collapsenav);
+        
         $(document).ready(function(){
             $("#tourist #spot #image-left .imagecontent .imagehover .info button").on("click",function(){
                 var x = $(this).attr("data-image");
--- assets/js/app.js
+++ assets/js/app.js
@@ -6,14 +6,7 @@ $(".navbar .container .navbar-header button").click(function(){
 $("#mobileview #navmenu button").click(function(){
     $("#mobileview #navmenu").removeClass("showmobile");
 });
-$("#bannerimage").slick({
-    arrows: false,
-    dots: true
-});
-$("section #about #bannerimage").slick({
-    arrows: false,
-    dots: true
-});
+
 function collapsenav(){
     if ($(".navbar").offset().top > 80){
         $('.navbar').addClass("scroll-change");
@@ -23,45 +16,3 @@ function collapsenav(){
 };
 $(window).scroll(collapsenav);
 $(document).ready(collapsenav);
-$(document).ready(function(){
-    $("#tourist #spot #image-left .imagecontent .imagehover .info button").on("click",function(){
-        var x = $(this).attr("data-image");
-        var des = $(this).prev().html();
-        $("#view img").attr("src",x);
-        $("#view #box2 p").html(des);
-        var num = $("#view").offset().top - 50 
-        $('html,body').animate({
-            scrollTop: num
-        }, 600);
-    });
-    $("#tourist #spot #image-right .imagecontent .imagehover .info button").on("click",function(){
-        var x = $(this).attr("data-image");
-        var des = $(this).prev().html();
-        $("#view img").attr("src",x);
-        $("#view #box2 p").html(des);
-        var num = $("#view").offset().top - 50 
-        $('html,body').animate({
-            scrollTop: num
-        }, 600);
-    });
-    $("#tourist #spot #center-image .imagehover .info button").on("click",function(){
-        var x = $(this).attr("data-image");
-        var des = $(this).prev().html();
-        $("#view img").attr("src",x);
-        $("#view #box2 p").html(des);
-        var num = $("#view").offset().top - 50 
-        $('html,body').animate({
-            scrollTop: num
-        }, 600);
-    });
-    $("#tourist #spot2 .image-content .imagehover .info button").on("click",function(){
-        var x = $(this).attr("data-image");
-        var des = $(this).prev().html();
-        $("#view img").attr("src",x);
-        $("#view #box2 p").html(des);
-        var num = $("#view").offset().top - 50 
-        $('html,body').animate({
-            scrollTop: num
-        }, 600);
-    });
-});
