d3d057bd40f84d24de7de0a31fce22145d0cbb16
marnidevlabs <marni@creativedevlabs.com>
2017-10-01 12:26:00 +0800

    tourist spot

--- assets/app/controller/homeController.js
+++ assets/app/controller/homeController.js
@@ -6,44 +6,53 @@ app.controller('homeController',[
     {
         $("section #about #bannerimage").slick({
             arrows: false,
-            dots: true
+            dots: true,
+            autoplay: true
         });
         $(document).ready(function(){
-            $("#tourist #spot #image-left .imagecontent .imagehover .info button").on("click",function(){
+            $("#tourist #spot #image-left .imagecontent .imagehover a").on("click",function(){
                 var x = $(this).attr("data-image");
-                var des = $(this).prev().html();
+                var url = $(this).attr("href");
+                $("#view #box2 a").attr("href",url);
+                var des = $(this).find("p").html()
                 $("#view img").attr("src",x);
-                $("#view #box2 p").html(des);
+                $("#view #box2 h3").html(des);
                 var num = $("#view").offset().top - 50 
                 $('html,body').animate({
                     scrollTop: num
                 }, 600);
             });
-            $("#tourist #spot #image-right .imagecontent .imagehover .info button").on("click",function(){
+            $("#tourist #spot #image-right .imagecontent .imagehover a").on("click",function(){
                 var x = $(this).attr("data-image");
-                var des = $(this).prev().html();
+                var url = $(this).attr("href");
+                $("#view #box2 a").attr("href",url);
+                var des = $(this).find("p").html()
                 $("#view img").attr("src",x);
-                $("#view #box2 p").html(des);
+                $("#view #box2 h3").html(des);
                 var num = $("#view").offset().top - 50 
                 $('html,body').animate({
                     scrollTop: num
                 }, 600);
             });
-            $("#tourist #spot #center-image .imagehover .info button").on("click",function(){
+            $("#tourist #spot #center-image .imagehover a").on("click",function(){
                 var x = $(this).attr("data-image");
-                var des = $(this).prev().html();
+                var url = $(this).attr("href");
+                $("#view #box2 a").attr("href",url);
+                var des = $(this).find("p").html()
                 $("#view img").attr("src",x);
-                $("#view #box2 p").html(des);
+                $("#view #box2 h3").html(des);
                 var num = $("#view").offset().top - 50 
                 $('html,body').animate({
                     scrollTop: num
                 }, 600);
             });
-            $("#tourist #spot2 .image-content .imagehover .info button").on("click",function(){
+            $("#tourist #spot2 .image-content .imagehover a").on("click",function(){
                 var x = $(this).attr("data-image");
-                var des = $(this).prev().html();
+                var url = $(this).attr("href");
+                $("#view #box2 a").attr("href",url);
+                var des = $(this).find("p").html()
                 $("#view img").attr("src",x);
-                $("#view #box2 p").html(des);
+                $("#view #box2 h3").html(des);
                 var num = $("#view").offset().top - 50 
                 $('html,body').animate({
                     scrollTop: num
