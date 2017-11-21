1813eacb4a3012c7b3f438032d4552c36c3826e1
CopterClest\Clint <copterclest@gmail.com>
2017-11-21 19:31:04 +0800

    no message

--- assets/app/controller/departmentSectionController.js
+++ assets/app/controller/departmentSectionController.js
@@ -9,29 +9,29 @@ app.controller('departmentSectionController',[
     )
     {
         $('.gallery').slick({
-            centerPadding: '60px',
-            slidesToShow: 3,
-            arrows:false,
-            responsive: [
-              {
-                breakpoint: 768,
-                settings: {
-                  arrows: false,
-                  centerMode: true,
-                  centerPadding: '40px',
-                  slidesToShow: 3
-                }
-              },
-              {
-                breakpoint: 480,
-                settings: {
-                  arrows: false,
-                  centerMode: true,
-                  centerPadding: '40px',
-                  slidesToShow: 1
-                }
+          //centerMode: true,
+          centerPadding: '60px',
+          slidesToShow: 3,
+          responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
               }
-            ]
-          });
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+              }
+            }
+          ]
+        });
     }
 ]);
